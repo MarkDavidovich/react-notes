030ead5babdd8ce740c4dbb528bbf3667f9d444a
Mark <mark.davidovich@gmail.com>
2026-02-06 18:16:50 +0200

    add search and filtering functionality

--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -1,24 +1,65 @@
 import style from "./NotesList.module.css";
 import Note from "../Note/Note";
+import { useState } from "react";
 
 const NotesList = ({ notes, onRemoveNote, onClickNote }) => {
+  const [searchedText, setSearchedText] = useState("");
+  const [filterCategory, setFilterCategory] = useState("");
+  // const [activeCategory, setActiveCategory] = useState(null);
+
+  const categories = ["personal", "work", "study", "other"];
+
+  const filteredNotes = notes.filter((n) => {
+    const textMatch = n.title.toLowerCase().includes(searchedText.toLowerCase()) || n.text.toLowerCase().includes(searchedText.toLowerCase());
+    const categoryMatch = filterCategory ? n.category === filterCategory : true;
+    return textMatch && categoryMatch;
+  });
+
+  const toggleFilter = (category) => {
+    setFilterCategory((prev) => (prev === category ? null : category));
+  };
+
   return (
-    <div className={style.container}>
-      {/* <div>Notes here? if yes, sort By a dropdown</div> */}
-      {notes.map((n) => (
-        <Note
-          key={n.id}
-          id={n.id}
-          title={n.title}
-          text={n.text}
-          dateCreated={n.dateCreated}
-          dateEdited={n.dateEdited}
-          onRemoveNote={onRemoveNote}
-          onClickNote={onClickNote}
-          color={n.color}
+    <>
+      <div className={style.query} style={notes.length > 1 ? { visibility: "visible" } : { visibility: "hidden" }}>
+        <input
+          type="text"
+          id={style.search}
+          placeholder="Search notes..."
+          onChange={(e) => {
+            setSearchedText(e.target.value);
+          }}
         />
-      ))}
-    </div>
+        <div className={style.filter}>
+          {categories.map((c) => (
+            <button
+              key={c}
+              className={`${filterCategory === c ? style.active : ""}`}
+              onClick={() => {
+                toggleFilter(c);
+              }}
+            >
+              {c}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className={style.container}>
+        {filteredNotes.map((n) => (
+          <Note
+            key={n.id}
+            id={n.id}
+            title={n.title}
+            text={n.text}
+            dateCreated={n.dateCreated}
+            dateEdited={n.dateEdited}
+            onRemoveNote={onRemoveNote}
+            onClickNote={onClickNote}
+            color={n.color}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
